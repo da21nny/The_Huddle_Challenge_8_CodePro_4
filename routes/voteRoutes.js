dce6c544d6bec5c301b09ed08f76f214775beebf
da21nny <edgarv21.ev@gmail.com>
2026-03-28 03:07:52 -0300

    refactor: simplify routing logic in index.js

--- routes/voteRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const voteController = require('../controllers/voteController');
-
-router.post('/topic/:id', voteController.voteTopic);
-router.post('/topic/:id/link/:lid', voteController.voteLink);
-
-module.exports = router;
