bc020992490f5b0be25d52ed56484056aeebbbd9
da21nny <edgarv21.ev@gmail.com>
2026-04-09 01:12:32 -0300

    feat: add link controller for handling nested resources

---
+++ src/controllers/linkController.js
@@ -0,0 +1,35 @@
+import * as linkModel from "../models/linkModel.js";
+
+const index = async (req, res) => {
+    const { topicId } = req.params;
+    const links = await linkModel.getLinksByTopic(topicId);
+    res.render("links/index", { links });
+};
+
+const store = async (req, res) => {
+    const { topicId } = req.params;
+    const { title, url } = req.body;
+    await linkModel.createLink(topicId, title, url);
+    res.redirect(`/topics/${topicId}`);
+};
+
+const vote = async (req, res) => {
+    const { id, topicId } = req.params;
+    await linkModel.voteLink(id);
+    res.redirect(`/topics/${topicId}`);
+};
+
+const destroy = async (req, res) => {
+    const { id, topicId } = req.params;
+    await linkModel.deleteLink(id);
+    res.redirect(`/topics/${topicId}`);
+};
+
+const update = async (req, res) => {
+    const { id, topicId } = req.params;
+    const { title, url } = req.body;
+    await linkModel.updateLink(id, title, url);
+    res.redirect(`/topics/${topicId}`);
+};
+
+export { index, store, vote, destroy, update };
