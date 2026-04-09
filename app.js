27aa8f69fa831f3c453f86615e99f884856d0ad7
da21nny <edgarv21.ev@gmail.com>
2026-04-09 01:13:24 -0300

    feat: integrate routes and database initialization in app entry point

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-import express, { json } from "express";
+import express, { json, urlencoded } from "express";
 import methodOverride from "method-override";
-import { v4 as uuidv4 } from "uuid";
+import { initDB } from "./src/config/db.js";
+import topicRoutes from "./src/routes/topicRoutes.js";
+import linkRoutes from "./src/routes/linkRoutes.js";
 
 const app = express();
 const PORT = 3000;
 
+// Initialize Database
+initDB();
+
+// Middleware
 app.use(json());
+app.use(urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
-app.set("views", "./views");
+app.set("views", "./src/views");
 
-const topics = [];
+// Routes
+app.use(topicRoutes);
+app.use(linkRoutes);
 
 app.get("/", (req, res) => {
-    res.render("index", { topics });
+    res.redirect("/topics");
 });
 
 app.listen(PORT, () => {
