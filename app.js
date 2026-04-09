470401f9e938c55f338d8bce03ae43a396f9e659
da21nny <edgarv21.dev@gmail.com>
2026-04-09 17:43:14 -0300

    feat: implement frontend voting/sorting logic and configure EJS views with static asset serving

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ import methodOverride from "method-override";
 import { initDB } from "./src/config/db.js";
 import topicRoutes from "./src/routes/topicRoutes.js";
 import linkRoutes from "./src/routes/linkRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 3000;
@@ -15,8 +20,17 @@ app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "ejs");
-app.set("views", "./src/views");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "src/views")));
+app.use(express.static(path.join(__dirname, "src/views/topics")));
+app.use(express.static(path.join(__dirname, "src/views/links")));
+app.use(express.static(path.join(__dirname, "src/controllers")));
+app.use(express.static(path.join(__dirname, "src/routes")));
+app.use(express.static(path.join(__dirname, "src/models")));
+app.use(express.static(path.join(__dirname, "src/config")));
 
 // Routes
 app.use(topicRoutes);
@@ -27,5 +41,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
