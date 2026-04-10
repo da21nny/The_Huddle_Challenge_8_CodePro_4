177b734045e4ddceffca09cbc8bafeda1486d992
da21nny <edgarv21.ev@gmail.com>
2026-04-09 21:38:46 -0300

    feat(routes): add link edit route

--- src/routes/linkRoutes.js
+++ src/routes/linkRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { index, store, vote, destroy, update } from "../controllers/linkController.js";
+import { index, store, vote, edit, destroy, update } from "../controllers/linkController.js";
 
 const router = Router();
 
 router.get("/topics/:topicId/links", index);
 router.post("/topics/:topicId/links", store);
+router.get("/topics/:topicId/links/:id/edit", edit);
 router.post("/topics/:topicId/links/:id/vote", vote);
 router.delete("/topics/:topicId/links/:id", destroy);
 router.put("/topics/:topicId/links/:id", update);
