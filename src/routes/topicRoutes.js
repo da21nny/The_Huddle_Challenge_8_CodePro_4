8f830d1e76f4c99ecf0fed2ad5b5f9fa320a54eb
da21nny <edgarv21.ev@gmail.com>
2026-04-09 21:38:34 -0300

    feat(routes): add topic edit route

--- src/routes/topicRoutes.js
+++ src/routes/topicRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { index, store, vote, show, destroy, update } from "../controllers/topicController.js";
+import { index, store, vote, show, edit, destroy, update } from "../controllers/topicController.js";
 
 const router = Router();
 
 router.get("/topics", index);
 router.post("/topics", store);
+router.get("/topics/:id/edit", edit);
 router.post("/topics/:id/vote", vote);
 router.get("/topics/:id", show);
 router.delete("/topics/:id", destroy);
