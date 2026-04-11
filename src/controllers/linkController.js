af3dbbdd77d325e682774ad25ae7a282085c404a
da21nny <edgarv21.ev@gmail.com>
2026-04-11 19:26:30 -0300

    feat(controllers): add try/catch error handling

--- src/controllers/linkController.js
+++ src/controllers/linkController.js
@@ -1,42 +1,66 @@
 import * as linkModel from "../models/linkModel.js";
 
 const index = async (req, res) => {
-    const { topicId } = req.params;
-    const links = await linkModel.getLinksByTopic(topicId);
-    res.render("links/index", { links });
+    try {
+        const { topicId } = req.params;
+        const links = await linkModel.getLinksByTopic(topicId);
+        res.render("links/index", { links });
+    } catch (error) {
+        res.render("error", { message: "Error al obtener los enlaces" });
+    }
 };
 
 const store = async (req, res) => {
-    const { topicId } = req.params;
-    const { title, url } = req.body;
-    await linkModel.createLink(topicId, title, url);
-    res.redirect(`/topics/${topicId}`);
+    try {
+        const { topicId } = req.params;
+        const { title, url } = req.body;
+        await linkModel.createLink(topicId, title, url);
+        res.redirect(`/topics/${topicId}`);
+    } catch (error) {
+        res.render("error", { message: "Error al crear el enlace" });
+    }
 };
 
 const vote = async (req, res) => {
-    const { id, topicId } = req.params;
-    await linkModel.voteLink(id);
-    const updatedLink = await linkModel.getLinkById(id);
-    res.json({ votes: updatedLink.votes });
+    try {
+        const { id, topicId } = req.params;
+        await linkModel.voteLink(id);
+        const updatedLink = await linkModel.getLinkById(id);
+        res.json({ votes: updatedLink.votes });
+    } catch (error) {
+        res.render("error", { message: "Error al votar el enlace" });
+    }
 };
 
 const edit = async (req, res) => {
-    const { id, topicId } = req.params;
-    const link = await linkModel.getLinkById(id);
-    res.render("links/edit", { link, topicId });
+    try {
+        const { id, topicId } = req.params;
+        const link = await linkModel.getLinkById(id);
+        res.render("links/edit", { link, topicId });
+    } catch (error) {
+        res.render("error", { message: "Error al editar el enlace" });
+    }
 };
 
 const destroy = async (req, res) => {
-    const { id, topicId } = req.params;
-    await linkModel.deleteLink(id);
-    res.redirect(`/topics/${topicId}`);
+    try {
+        const { id, topicId } = req.params;
+        await linkModel.deleteLink(id);
+        res.redirect(`/topics/${topicId}`);
+    } catch (error) {
+        res.render("error", { message: "Error al eliminar el enlace" });
+    }
 };
 
 const update = async (req, res) => {
-    const { id, topicId } = req.params;
-    const { title, url } = req.body;
-    await linkModel.updateLink(id, title, url);
-    res.redirect(`/topics/${topicId}`);
+    try {
+        const { id, topicId } = req.params;
+        const { title, url } = req.body;
+        await linkModel.updateLink(id, title, url);
+        res.redirect(`/topics/${topicId}`);
+    } catch (error) {
+        res.render("error", { message: "Error al actualizar el enlace" });
+    }
 };
 
 export { index, store, vote, edit, destroy, update };
--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -2,47 +2,75 @@ import * as topicModel from "../models/topicModel.js";
 import * as linkModel from "../models/linkModel.js";
 
 const index = async (req, res) => {
-    const topics = await topicModel.getAllTopics();
-    res.render("topics/index", { topics });
+    try {
+        const topics = await topicModel.getAllTopics();
+        res.render("topics/index", { topics });
+    } catch (error) {
+        res.render("error", { message: "Error al obtener los temas" });
+    }
 };
 
 const store = async (req, res) => {
-    const { title, description } = req.body;
-    await topicModel.createTopic(title, description);
-    res.redirect("/topics");
+    try {
+        const { title, description } = req.body;
+        await topicModel.createTopic(title, description);
+        res.redirect("/topics");
+    } catch (error) {
+        res.render("error", { message: "Error al crear el tema" });
+    }
 };
 
 const vote = async (req, res) => {
-    const { id } = req.params;
-    await topicModel.voteTopic(id);
-    const updatedTopic = await topicModel.getTopicById(id);
-    res.json({ votes: updatedTopic.votes });
+    try {
+        const { id } = req.params;
+        await topicModel.voteTopic(id);
+        const updatedTopic = await topicModel.getTopicById(id);
+        res.json({ votes: updatedTopic.votes });
+    } catch (error) {
+        res.render("error", { message: "Error al votar el tema" });
+    }
 };
 
 const show = async (req, res) => {
-    const { id } = req.params;
-    const topic = await topicModel.getTopicById(id);
-    const links = await linkModel.getLinksByTopic(id);
-    res.render("topics/show", { topic, links });
+    try {
+        const { id } = req.params;
+        const topic = await topicModel.getTopicById(id);
+        const links = await linkModel.getLinksByTopic(id);
+        res.render("topics/show", { topic, links });
+    } catch (error) {
+        res.render("error", { message: "Error al obtener el tema" });
+    }
 };
 
 const edit = async (req, res) => {
-    const { id } = req.params;
-    const topic = await topicModel.getTopicById(id);
-    res.render("topics/edit", { topic });
+    try {
+        const { id } = req.params;
+        const topic = await topicModel.getTopicById(id);
+        res.render("topics/edit", { topic });
+    } catch (error) {
+        res.render("error", { message: "Error al editar el tema" });
+    }
 };
 
 const destroy = async (req, res) => {
-    const { id } = req.params;
-    await topicModel.deleteTopic(id);
-    res.redirect("/topics");
+    try {
+        const { id } = req.params;
+        await topicModel.deleteTopic(id);
+        res.redirect("/topics");
+    } catch (error) {
+        res.render("error", { message: "Error al eliminar el tema" });
+    }
 };
 
 const update = async (req, res) => {
-    const { id } = req.params;
-    const { title, description } = req.body;
-    await topicModel.updateTopic(id, title, description);
-    res.redirect("/topics");
+    try {
+        const { id } = req.params;
+        const { title, description } = req.body;
+        await topicModel.updateTopic(id, title, description);
+        res.redirect("/topics");
+    } catch (error) {
+        res.render("error", { message: "Error al actualizar el tema" });
+    }
 };
 
 export { index, store, vote, show, edit, destroy, update };
