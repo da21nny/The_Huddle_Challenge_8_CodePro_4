8e1201dc38a75c0cd303c086f1cf7422b3a9e726
da21nny <edgarv21.ev@gmail.com>
2026-04-11 21:35:22 -0300

    docs: add descriptive comments to linkModel functions

--- src/models/linkModel.js
+++ src/models/linkModel.js
@@ -1,5 +1,6 @@
 import db from "../config/db.js";
 
+// Obtiene todos los enlaces de un topic, ordenados por votos de mayor a menor
 const getLinksByTopic = (topicId) => {
     return new Promise((resolve, reject) => {
         db.all("SELECT * FROM links WHERE topic_id = ? ORDER BY votes DESC", [topicId], (err, rows) => {
@@ -9,6 +10,7 @@ const getLinksByTopic = (topicId) => {
     });
 };
 
+// Obtiene un enlace por su ID
 const getLinkById = (id) => {
     return new Promise((resolve, reject) => {
         db.get("SELECT * FROM links WHERE id = ?", [id], (err, row) => {
@@ -18,6 +20,7 @@ const getLinkById = (id) => {
     });
 };
 
+// Crea un nuevo enlace asociado a un topic y retorna el objeto creado
 const createLink = (topicId, title, url) => {
     return new Promise((resolve, reject) => {
         db.run("INSERT INTO links (topic_id, title, url) VALUES (?, ?, ?)", [topicId, title, url], function (err) {
@@ -27,6 +30,7 @@ const createLink = (topicId, title, url) => {
     });
 };
 
+// Incrementa en 1 el conteo de votos de un enlace
 const voteLink = (id) => {
     return new Promise((resolve, reject) => {
         db.run("UPDATE links SET votes = votes + 1 WHERE id = ?", [id], function (err) {
@@ -36,6 +40,7 @@ const voteLink = (id) => {
     });
 };
 
+// Elimina un enlace por su ID y retorna la cantidad de filas afectadas
 const deleteLink = (id) => {
     return new Promise((resolve, reject) => {
         db.run("DELETE FROM links WHERE id = ?", [id], function (err) {
@@ -45,6 +50,7 @@ const deleteLink = (id) => {
     });
 };
 
+// Actualiza el título y URL de un enlace existente
 const updateLink = (id, title, url) => {
     return new Promise((resolve, reject) => {
         db.run("UPDATE links SET title = ?, url = ? WHERE id = ?", [title, url, id], function (err) {
